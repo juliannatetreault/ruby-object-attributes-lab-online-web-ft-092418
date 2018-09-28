c66bde5112d1af05dfa9c3f644b3a0259da3c5c9
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-28 10:05:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,12 @@
 class Person 
-  def name(name)
-    @name = name 
+  def name
+    @name 
   end
   def name=(persons_name)
     @name = persons_name
   end
-  def job(job)
-    @job = job
+  def job
+    @job
   end 
   def job=(persons_job)
     @job = persons_job
